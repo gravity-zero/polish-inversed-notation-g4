a2fd206d90f471e7a470efc424f19a5a6e7fbf11
bpaquier <bastien.paquier@gmail.com>
2022-05-04 11:42:44 +0200

    handle simple calculs

---
+++ spa/src/core/index.spec.ts
@@ -0,0 +1,124 @@
+import {
+  resolveReversePolishExpression,
+  parseExpression,
+  calculate,
+} from "./index";
+
+describe("Calculate / Main test", function () {
+  it("Simple addition", function () {
+    //GIVEN
+    const expression = "1 2 +";
+
+    //WHEN
+    const actual = resolveReversePolishExpression(expression);
+
+    //THEN
+    const expected = 3;
+
+    expect(actual).toEqual(expected);
+  });
+  it("Simple subtraction", function () {
+    //GIVEN
+    const expression = "1 2 -";
+
+    //WHEN
+    const actual = resolveReversePolishExpression(expression);
+
+    //THEN
+    const expected = -1;
+
+    expect(actual).toEqual(expected);
+  });
+  it("Simple multiplication", function () {
+    //GIVEN
+    const expression = "3 5 *";
+
+    //WHEN
+    const actual = resolveReversePolishExpression(expression);
+
+    //THEN
+    const expected = 15;
+
+    expect(actual).toEqual(expected);
+  });
+  it("Simple division", function () {
+    //GIVEN
+    const expression = "10 5 /";
+
+    //WHEN
+    const actual = resolveReversePolishExpression(expression);
+
+    //THEN
+    const expected = 2;
+
+    expect(actual).toEqual(expected);
+  });
+  it("multiplication with negative number", function () {
+    //GIVEN
+    const expression = "10 5 NEGATE *";
+
+    //WHEN
+    const actual = resolveReversePolishExpression(expression);
+
+    //THEN
+    const expected = -50;
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("utils", function () {
+  it("Transform expression in exploitable array", function () {
+    //GIVEN
+    const expression = "1 2 +";
+
+    //WHEN
+    const actual = parseExpression(expression);
+
+    //THEN
+    const expected = [1, 2, "+"];
+
+    expect(actual).toEqual(expected);
+  });
+  it("Calculate  1 + 3", function () {
+    //GIVEN
+    const number1 = 1;
+    const number2 = 3;
+    const operator = "+";
+
+    //WHEN
+    const actual = calculate(number1, number2, operator);
+
+    //THEN
+    const expected = 4;
+
+    expect(actual).toEqual(expected);
+  });
+  it("Calculate  1 ~ 3", function () {
+    //GIVEN
+    const number1 = 1;
+    const number2 = 3;
+    const operator = "~";
+
+    //WHEN
+    //@ts-ignore
+    const actual = calculate(number1, number2, operator);
+
+    //THEN
+    const expected = null;
+
+    expect(actual).toEqual(expected);
+  });
+  it("Transform expression in exploitable array", function () {
+    //GIVEN
+    const expression = "1 2 NEGATE +";
+
+    //WHEN
+    const actual = parseExpression(expression);
+
+    //THEN
+    const expected = [1, -2, "+"];
+
+    expect(actual).toEqual(expected);
+  });
+});
---
+++ spa/src/core/index.ts
@@ -0,0 +1,73 @@
+import { isNumber } from "lodash";
+import { parse } from "path";
+import { sortAndDeduplicateDiagnostics } from "typescript";
+
+type Operator = "*" | "/" | "+" | "-";
+const operators = ["*", "/", "+", "-", "NEGATE"];
+
+export function resolveReversePolishExpression(
+  expression: string
+): Number | null {
+  const formatedExp = parseExpression(expression);
+
+  const firstOperatorIndex: number = formatedExp?.findIndex(
+    (el) => typeof el === "string" && operators?.includes(el)
+  );
+
+  const number1 = formatedExp[firstOperatorIndex - 2];
+  const number2 = formatedExp[firstOperatorIndex - 1];
+  const operator = formatedExp[firstOperatorIndex];
+
+  if (isNumber(number1) && isNumber(number2) && typeof operator === "string") {
+    return calculate(number1, number2, operator);
+  }
+
+  return null;
+}
+
+export function parseExpression(expression: string): (number | string)[] {
+  const negateIndexes: number[] = [];
+  const parsedExpression: (number | string)[] = expression
+    ?.split(" ")
+    ?.map((el) => (isNaN(parseInt(el)) ? el : parseInt(el)))
+    ?.map((element, i) => {
+      if (element === "NEGATE") {
+        negateIndexes.push(i);
+        return "";
+      }
+      return element;
+    })
+    ?.filter(
+      (el) =>
+        (typeof el === "string" && operators?.includes(el)) || isNumber(el)
+    );
+
+  if (negateIndexes?.length > 0 && parse) {
+    negateIndexes?.forEach((index) => {
+      if (typeof parsedExpression[index - 1] === "number") {
+        parsedExpression.splice(index - 1, 1, parsedExpression[index - 1] * -1);
+      }
+    });
+  }
+
+  return parsedExpression;
+}
+
+export function calculate(
+  number1: number,
+  number2: number,
+  operator: string
+): Number | null {
+  switch (operator) {
+    case "+":
+      return number1 + number2;
+    case "-":
+      return number1 - number2;
+    case "/":
+      return number1 / number2;
+    case "*":
+      return number1 * number2;
+    default:
+      return null;
+  }
+}
